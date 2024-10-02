b5239d465ced688b93c1d2d78aa97728758798fd
Kyrylo Rusakov <Kyrylo_Rusakov@epam.com>
2024-10-02 11:45:00 +0300

    add counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import './index.scss';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div className="App">
       <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h2>Сounter:</h2>
+        <h1>{count}</h1>
+        <button className="minus" onClick={decrement}>- Minus</button>
+        <button className="plus" onClick={increment}>Plus +</button>
       </div>
     </div>
   );
